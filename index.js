b353871d020fbbd65cf67713fd55016c684d4cf3
Maor Attias <maorattias17@gmail.com>
2022-11-01 18:12:54 +0200

    files added. function for read flies working. function to write files now working

---
+++ index.js
@@ -0,0 +1,50 @@
+import log from '@ajar/marker';
+import fs from 'fs/promises';
+
+async function myReadFile() {
+    try {
+      const data = await fs.readdir('./LEADS');
+      const output = []
+ 
+      for (let file of data){
+        const fileContent = await fs.readFile(`./LEADS/${file}`,'utf-8')
+        const lines = fileContent.split('\r\n')
+        for (let user of lines){
+            let [id, fullName, email] = user.split(',')
+            fullName = fullName.split(/"/g)
+            
+            const userObj = {
+                id,
+                fullName: fullName[1],
+                email
+            }
+            output.push(userObj)
+ 
+        } 
+    }
+    return output
+
+
+    } catch (err) {
+      console.log(err);
+    }
+  }
+// myReadFile()
+
+
+  async function myWriteFile(){
+    const data = await myReadFile()
+    let jsonData = JSON.stringify(data)
+    fs.writeFile('./result.json',jsonData,function (err) {
+        if (err) {
+            console.log(err)
+            return;
+        }
+        else{
+            console.log('File has been written succesfuly')
+        }
+    })
+  }
+
+myWriteFile()
+
